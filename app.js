100b757074ec9502c112e65e9cfba5a635d456d2
snehakpr2005 <sneha2005kapoor@gmail.com>
2026-05-05 11:10:07 +0530

    .env file updated

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import reportRoutes from "./routes/reportRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use("/api", bookingRoutes);
+app.use("/api", reportRoutes);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
